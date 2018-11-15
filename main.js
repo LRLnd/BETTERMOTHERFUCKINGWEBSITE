eaf7b0015bfc32fcc20944a7153a1f8487e88a49
Edmilson <edi_varela@hotmail.fr>
2018-11-15 19:43:06 +0100

    ajout de css plus popup link

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-setTimeout(function() {  
-alert('on a réussis à mettre un compte à rebour au message!! Maintenant on fait comment pour mettre un lien à un popup, avec le javascript???');
-}, 10000);
+/* setTimeout(function() {  
+window.confirm('You want to go on our pornosite? If it yes press OK!! ');
+}, 10000); */
+/*var confirme= confirm('Veux tu rejoindre notre site Porno sxxvixx ??! Clique sur <OK>')
+if (confirme) { window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ"); }
+else { alert('Tu sais pas ce que tu rates mec'); } */
+function monFichierPopup() { 
+var confirme= confirm('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please!! Allez petit coquin clique <OK>');
+if (confirme) { window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ"); }
+else { alert('Tu sais pas ce que tu rates mon choux!'); } }
+  
+setTimeout(function() { monFichierPopup() } , 10000);
+
